af3ddf91d462513bafcc25d5e1d872329ea139fa
CodeFullStack-dev <codefullstack1214@gmail.com>
2026-02-05 20:48:18 +0700

    Install and config Tailwind css

--- Quan_Ly_Hoc_Sinh_Frontend/src/App.tsx
+++ Quan_Ly_Hoc_Sinh_Frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="flex items-center justify-center min-h-screen bg-slate-900">
+      <div className="p-10 bg-white rounded-xl shadow-2xl">
+        <h1 className="text-3xl font-bold text-blue-600 underline">
+          Tailwind đã hoạt động thành công!
+        </h1>
+        <p className="mt-4 text-gray-600">
+          Hệ thống Quản lý Học sinh - Frontend
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
+
